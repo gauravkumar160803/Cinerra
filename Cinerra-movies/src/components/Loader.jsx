9ee1c6b2d4fc27b32d646d17aa4ff72e60278956
Gaurav Kumar <ghoul@Mac.lan>
2026-04-14 19:32:50 +0530

    oke

--- Cinerra-movies/src/components/Loader.jsx
+++ Cinerra-movies/src/components/Loader.jsx
@@ -1,6 +1,6 @@
 export default function Loader() {
   return (
-    <div className="flex justify-center items-center h-[60vh]">
+    <div className="flex justify-center items-center min-h-[calc(100vh-80px)]">
       <div className="w-10 h-10 border-4 border-gray-300 border-t-[#8a3357] rounded-full animate-spin"></div>
     </div>
   );
