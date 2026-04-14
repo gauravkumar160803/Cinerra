f94e333e4b42ec7f4be0c7f58973fc1d980ed32d
Gaurav Kumar <ghoul@Mac.lan>
2026-04-14 19:52:04 +0530

    updated loader with animated logo2

--- Cinerra-movies/src/components/Loader.jsx
+++ Cinerra-movies/src/components/Loader.jsx
@@ -3,7 +3,7 @@ export default function Loader() {
     <div className="flex justify-center items-center min-h-[calc(100vh-80px)] bg-gray-100">
       
       <img
-        src="/Logos/NavbarLogo.png"
+        src="/Logos/logo-transparent-png.png"
         alt="Loading..."
         className="w-16 h-16 animate-[float_2s_ease-in-out_infinite] drop-shadow-lg"
       />
